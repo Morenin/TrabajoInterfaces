01aef3297592dd3a987d530bc485aec958df2a25
Morenin <fjmoreno22@gmail.com>
2020-10-27 13:04:27 +0100

    prueba2

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name Hola
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
