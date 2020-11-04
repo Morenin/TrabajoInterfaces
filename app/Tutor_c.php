e9bb04c75035eaf0ce86417008dec3db5acc12c5
Morenin <fjmoreno22@gmail.com>
2020-11-04 09:19:34 +0100

    Controlladores

--- app/Tutor_c.php
+++ app/Tutor_c.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tutor_c extends Model
 {
-    //
+    protected $table='tutor_c';
+    protected $fillable = [
+        'name','firstname','email','phone','cycle_id','deleted'
+    ];
 }
