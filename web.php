d56fe5e6fb44598dfe4b8344c1a9a9bdd29d1437
JaviTravesi77 <73529007+JaviTravesi77@users.noreply.github.com>
2020-11-11 15:56:16 +0100

    errores cycles

---
+++ web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function() {
+    return redirect(route('login'));
+});
+Route::get('/starter', function() {
+    return view('starter');
+});
+
+Auth::routes(['verify' => false, 'reset' => false]);
+
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+});
+
+Route::get('/Usuario', 'UserController@index')->name('usuario');
+Route::resource('User','UserController');
+
+Route::get('/Usuario', 'UserController@index')->name('usuario');
+Route::resource('User','UserController');
+
+Route::get('/Cycle', 'CycleController@index')->name('cycle');
+Route::resource('Cycles', 'CyclesController');
