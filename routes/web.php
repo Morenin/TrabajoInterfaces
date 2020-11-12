87bf2b710672b40e470582be9b8d5777995cdbb9
AliHedgie <49725749+AliHedgie@users.noreply.github.com>
2020-11-12 10:30:32 +0100

    Update web.php
    He añadido mi ruta y la suya.

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,9 @@ Route::middleware('auth')->group(function() {
 
 Route::get('/Usuario', 'UserController@index')->name('usuario');
 Route::resource('User','UserController');
+
+Route::get('/empresas', 'EnterpriseController@index')->name('empresa');
+Route::resource('Enterprise', 'EnterpriseController');
+
+Route::get('/Cycle', 'CycleController@index')->name('cycle');
+Route::resource('Cycles', 'CyclesController');
