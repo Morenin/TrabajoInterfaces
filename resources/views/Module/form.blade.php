2cc1bb4172d1ca1f7ce39c3624cab7616f0e7cf4
AliHedgie <49725749+AliHedgie@users.noreply.github.com>
2020-11-13 14:14:14 +0100

    Vistas módulos
    No lo he probado, digo yo que funcionará y si no lo arreglaré más tarde

---
+++ resources/views/Module/form.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Cycle:</strong>
+            {!! Form::cycle_id('cycle_id', null, array('placeholder' => 'Cycle','class' => 'form-control')) !!}
+        </div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
---
+++ resources/views/Module/index.blade.php
@@ -0,0 +1,38 @@
+@extends('dashboard')
+@section('content')
+<div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Lista de Modulos</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('Module.create') }}"> Crear nuevo módulo</a>
+            </div>
+        </div>
+    </div>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <table class="table table-bordered">
+        <tr>
+          <th>Nombre</th>
+          <th>Ciclo</th>
+          <th width="280px">Operation</th>
+        </tr>
+    @foreach ($modules as $Module)
+    <tr>
+          <td>{{$Module -> name }}</td>
+          <td>{{$Module -> cycle_id }}</td>
+        <td>
+            <a class="btn btn-primary" href="{{ route('Module.edit',$Module->id) }}">Edit</a>
+            {!! Form::open(['method' => 'DELETE','route' => ['Module.destroy', $Module->id],'style'=>'display:inline']) !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+            {!! Form::close() !!}
+        </td>
+    </tr>
+    @endforeach
+    </table>
+    {!! $enterprises->render() !!}
+@endsection
